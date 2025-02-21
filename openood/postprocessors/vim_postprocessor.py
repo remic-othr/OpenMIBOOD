e1c0cdc48231c21a704432149c6ec6543c0057ca
Max Gutbrod <gutbrodmax@gmx.de>
2025-02-21 15:35:39 +0000

    Postprocessor ViM: Bugfix; During hyperparameter search the NS and alpha variables were not getting recalculated for each different value of dim

--- openood/postprocessors/vim_postprocessor.py
+++ openood/postprocessors/vim_postprocessor.py
@@ -18,6 +18,8 @@ class VIMPostprocessor(BasePostprocessor):
         self.args_dict = self.config.postprocessor.postprocessor_sweep
         self.dim = self.args.dim
         self.setup_flag = False
+        self.feature_id_train = []
+        self.logit_id_train = None
 
     def setup(self, net: nn.Module, id_loader_dict, ood_loader_dict):
         if not self.setup_flag:
@@ -26,7 +28,6 @@ class VIMPostprocessor(BasePostprocessor):
             with torch.no_grad():
                 self.w, self.b = net.get_fc()
                 print('Extracting id training feature')
-                feature_id_train = []
                 for batch in tqdm(id_loader_dict['train'],
                                   desc='Setup: ',
                                   position=0,
@@ -34,23 +35,18 @@ class VIMPostprocessor(BasePostprocessor):
                     data = batch['data'].cuda()
                     data = data.float()
                     _, feature = net(data, return_feature=True)
-                    feature_id_train.append(feature.cpu().numpy())
-                feature_id_train = np.concatenate(feature_id_train, axis=0)
-                logit_id_train = feature_id_train @ self.w.T + self.b
+                    self.feature_id_train.append(feature.cpu().numpy())
+                self.feature_id_train = np.concatenate(self.feature_id_train, axis=0)
+                self.logit_id_train = self.feature_id_train @ self.w.T + self.b
 
             self.u = -np.matmul(pinv(self.w), self.b)
+            
             ec = EmpiricalCovariance(assume_centered=True)
-            ec.fit(feature_id_train - self.u)
-            eig_vals, eigen_vectors = np.linalg.eig(ec.covariance_)
-            self.NS = np.ascontiguousarray(
-                (eigen_vectors.T[np.argsort(eig_vals * -1)[self.dim:]]).T)
+            ec.fit(self.feature_id_train - self.u)
 
-            vlogit_id_train = norm(np.matmul(feature_id_train - self.u,
-                                             self.NS),
-                                   axis=-1)
-            self.alpha = logit_id_train.max(
-                axis=-1).mean() / vlogit_id_train.mean()
-            print(f'{self.alpha=:.4f}')
+            self.eig_vals, self.eigen_vectors = np.linalg.eig(ec.covariance_)
+            self.calculate_params()
+
 
             self.setup_flag = True
         else:
@@ -60,7 +56,7 @@ class VIMPostprocessor(BasePostprocessor):
     def postprocess(self, net: nn.Module, data: Any):
         _, feature_ood = net.forward(data, return_feature=True)
         feature_ood = feature_ood.cpu()
-        logit_ood = feature_ood @ self.w.T + self.b
+        logit_ood = torch.tensor(feature_ood @ self.w.T + self.b) # torch.tensor is necessary because of the metatensor with the OASIS3 dataset
         _, pred = torch.max(logit_ood, dim=1)
         energy_ood = logsumexp(logit_ood.numpy(), axis=-1)
         vlogit_ood = norm(np.matmul(feature_ood.numpy() - self.u, self.NS),
@@ -68,8 +64,15 @@ class VIMPostprocessor(BasePostprocessor):
         score_ood = -vlogit_ood + energy_ood
         return pred, torch.from_numpy(score_ood)
 
+    def calculate_params(self):
+        self.NS = np.ascontiguousarray((self.eigen_vectors.T[np.argsort(self.eig_vals * -1)[self.dim:]]).T)
+        vlogit_id_train = norm(np.matmul(self.feature_id_train - self.u, self.NS), axis=-1)
+        self.alpha = self.logit_id_train.max(axis=-1).mean() / vlogit_id_train.mean()
+        print(f'dim: {self.dim} results in {self.alpha=:}')
+
     def set_hyperparam(self, hyperparam: list):
         self.dim = hyperparam[0]
+        self.calculate_params()
 
     def get_hyperparam(self):
         return self.dim
