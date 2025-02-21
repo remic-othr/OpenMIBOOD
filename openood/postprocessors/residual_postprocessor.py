5187ea1964e0a766b5cdaffc3cbe27b73c6c8925
Max Gutbrod <gutbrodmax@gmx.de>
2025-02-21 15:23:45 +0000

    Postprocessor Residual: Included missing Hyperparameter-Search; torch.as_tensor is necessary because the OASIS3 data is loaded as 'metatensor', which is incompatible with the utilized operators

--- openood/postprocessors/residual_postprocessor.py
+++ openood/postprocessors/residual_postprocessor.py
@@ -14,7 +14,15 @@ class ResidualPostprocessor(BasePostprocessor):
     def __init__(self, config):
         super().__init__(config)
         self.args = self.config.postprocessor.postprocessor_args
+        self.args_dict = self.config.postprocessor.postprocessor_sweep
         self.dim = self.args.dim
+    
+    def set_hyperparam(self, hyperparam: list):
+        self.dim = hyperparam[0]
+        self._update_internals()
+
+    def get_hyperparam(self):
+        return self.dim
 
     def setup(self, net: nn.Module, id_loader_dict, ood_loader_dict):
         net.eval()
@@ -48,17 +56,22 @@ class ResidualPostprocessor(BasePostprocessor):
         self.u = -np.matmul(pinv(self.w), self.b)
         ec = EmpiricalCovariance(assume_centered=True)
         ec.fit(feature_id_train - self.u)
-        eig_vals, eigen_vectors = np.linalg.eig(ec.covariance_)
+        self.eig_vals, self.eigen_vectors = np.linalg.eig(ec.covariance_)
         self.NS = np.ascontiguousarray(
-            (eigen_vectors.T[np.argsort(eig_vals * -1)[self.dim:]]).T)
+            (self.eigen_vectors.T[np.argsort(self.eig_vals * -1)[self.dim:]]).T)
 
         self.score_id = -norm(np.matmul(feature_id_val - self.u, self.NS),
                               axis=-1)
+        
+    def _update_internals(self):
+        self.NS = np.ascontiguousarray(
+            (self.eigen_vectors.T[np.argsort(self.eig_vals * -1)[self.dim:]]).T)
+
 
     @torch.no_grad()
     def postprocess(self, net: nn.Module, data: Any):
         _, feature_ood = net(data, return_feature=True)
-        logit_ood = feature_ood.cpu() @ self.w.T + self.b
+        logit_ood = torch.as_tensor(feature_ood.cpu() @ self.w.T + self.b)
         _, pred = torch.max(logit_ood, dim=1)
         score_ood = -norm(np.matmul(feature_ood.cpu() - self.u, self.NS),
                           axis=-1)
