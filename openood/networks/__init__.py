38074cf76b8d43f2ee4cf9ffa2a59076283b04d7
Max Gutbrod <gutbrodmax@gmx.de>
2025-03-07 10:30:49 +0000

    Added the r2plus1d_18 network (used for the OASIS-3 benchmark)

--- openood/networks/__init__.py
+++ openood/networks/__init__.py
@@ -8,6 +8,7 @@ from .densenet import DenseNet3
 from .resnet18_32x32 import ResNet18_32x32
 from .resnet18_224x224 import ResNet18_224x224
 from .resnet50 import ResNet50
+from .r2plus1d import r2plus1d_18
 from .utils import get_network
 from .wrn import WideResNet
 from .swin_t import Swin_T
