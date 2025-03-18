0836e74699be1ba93238de3fb368baa376d9b290
Max Gutbrod <gutbrodmax@gmx.de>
2025-03-18 09:57:04 +0000

    Fixed link to the Endovis dataset

--- scripts/download/OpenMIBOOD/download_phakir_endovis2018.py
+++ scripts/download/OpenMIBOOD/download_phakir_endovis2018.py
@@ -3,9 +3,9 @@ import os
 import zipfile
 from tqdm import tqdm
 
-zip_paths = ['Endovis2018_1.zip', 'Endovis2018_2.zip']
-
 script_dir = os.path.dirname(os.path.abspath(__file__))
+zip_paths = [f'{script_dir}/Endovis2018_1.zip', f'{script_dir}/Endovis2018_2.zip']
+
 base_path = f'{script_dir}/../../../data/phakir/near/Endovis2018'
 
 if os.path.exists(zip_paths[0]) and os.path.exists(zip_paths[1]):
@@ -23,6 +23,8 @@ if os.path.exists(zip_paths[0]) and os.path.exists(zip_paths[1]):
         shutil.copytree(input_path, output_path)
     
     print('Endovis2018: Processing complete')
-else:
-    print('To acquire the Endovis2018 data, please register an account at https://endovissub2018-roboticinstrumentsegmentation.grand-challenge.org/. After joining the challenge, the data can be downloaded.\nPlease make sure that you only download the test dataset consisting of 4 sequences.\nAfter the download is complete, rename the two files to Endovis2018_1.zip and Endovis2018_2.zip, move them to this directory and run this script again.')
+else:                                                                    
+    print('To acquire the Endovis2018 data, please register an account at https://endovissub2018-roboticscenesegmentation.grand-challenge.org/.')
+    print('After joining the challenge, the data can be downloaded.\nPlease make sure that you only download the test dataset consisting of 4 sequences.')
+    print('After the download is complete, rename the two files to Endovis2018_1.zip and Endovis2018_2.zip, move them to this directory and run this script again.')
 
